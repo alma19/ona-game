fbb537cdfd2432d1a39187525dc2419e905af371
alma19 <almawashington@gmail.com>
2017-10-07 01:37:58 -0400

    Arrival.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
 var total = 0;
 
-function test(){
-  total += 300;
+// function test(){
+//   total += 300;
+//   document.getElementById('total-display').innerHTML = total;
+//   $('.question').css('display','none');
+// }
+
+function none(){
+  $('.question1').css('display','none');
+}
+
+function studentMember(){
+  total += 150;
   document.getElementById('total-display').innerHTML = total;
   $('.question').css('display','none');
+}
+
+function studentNonMember(){
+  total += 175;
+  document.getElementById('total-display').innerHTML = total;
+  $('.question').css('display','none');
+}
+
+function nonStudentMember(){
+  total += 659;
+  document.getElementById('total-display').innerHTML = total;
+  $('.question').css('display','none');
+}
+
+function nonStudentNonMember(){
+  total += 989;
+  document.getElementById('total-display').innerHTML = total;
+  $('.question').css('display','none');
+}
+
+
+function banquet(){
+  total += 30;
+  document.getElementById('total-display').innerHTML = total;
+  $('.question3').css('display','none');
 }
